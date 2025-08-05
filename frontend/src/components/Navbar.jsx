d4563bc2759d943098259571f38a6ed957615434
Piyush Yadav <piyushyadav20045@gmail.com>
2025-08-05 23:42:07 +0530

    feat: integrated tailwind css and made navbar and home section

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,71 @@
+import { Leaf, CircleUser, Menu, CircleX } from "lucide-react";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const Navlinks = [
+    { href: "#", label: "Home" },
+    { href: "camp", label: "Campgrounds" },
+    { href: "#new", label: "New Campgrounds" },
+  ];
+  return (
+    <div className="px-2 lg:px-12 2xl:px-52 p-4 border-b flex justify-between items-center text-white">
+      {/* LOGO */}
+      <span className="flex items-center text-3xl text-white font-extrabold font-logo">
+        {<Leaf color="green" size={28} />}CampX
+      </span>
+      {/* Desktop Navbar Links*/}
+      <nav className="hidden w-full md:flex justify-center gap-10 text-lg font-medium ">
+        {Navlinks.map((link) => (
+          <a
+            key={link.href}
+            className="hover:text-green-400 cursor-pointer active:scale-95 transition ease-in-out duration-200"
+            href={link.href}
+          >
+            {link.label}
+          </a>
+        ))}
+      </nav>
+      {/* SignUp */}
+      <a
+        href="#"
+        className="hidden md:flex gap-1 w-40 text-lg font-medium hover:text-green-400 cursor-pointer active:scale-95 transition ease-in-out duration-200"
+      >
+        <CircleUser />
+        Sign in/up
+      </a>
+      {/* MOBILE MENU Btn */}
+      <button
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+        className="md:hidden pt-1 rounded-full active:bg-gray-700"
+      >
+        {isMenuOpen ? <CircleX size={28} /> : <Menu size={28} />}
+      </button>
+      {/* MOBILE MENU links */}
+      <nav
+        className={`md:hidden w-full p-6 absolute top-18 left-0 flex flex-col rounded space-y-6 backdrop-blur-2xl text-center text-lg font-medium opacity-0 transition-all duration-200 ease-out scale-95
+        ${isMenuOpen ? "opacity-100 scale-100" : "pointer-events-none"}`}
+      >
+        {Navlinks.map((link) => (
+          <a
+            key={link.href}
+            className="hover:text-green-400 cursor-pointer"
+            href={link.href}
+          >
+            {link.label}
+          </a>
+        ))}
+        <a
+          href="#"
+          className="flex gap-1 text-lg mx-auto font-medium hover:text-green-400"
+        >
+          <CircleUser />
+          Sign in/up
+        </a>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import { Search } from "lucide-react";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <div className="">
+      <div className="px-3 md:px-0 h-screen bg-[url(bgImg.jpg)] bg-gray-950 bg-cover bg-center">
+        {/* NAVBAR */}
+        <Navbar />
+
+        {/* HERO Section */}
+        <div className="mt-8 max-h-fit flex flex-col justify-center items-center text-white text-center md:text-start">
+          <img src="camp.svg" className="h-44 w-44 text-white " />
+          <h1 className="text-4xl md:text-6xl font-extrabold mb-4">
+            Find Your Perfect Escape.
+          </h1>
+          <p className="text-lg md:text-xl max-w-2xl mb-8">
+            Discover and book unique campgrounds, from serene national parks to
+            hidden backcountry spots.
+          </p>
+          {/* SEARCH */}
+          <div className="w-full max-w-2xl flex justify-center gap-2 text-white px-5 py-3 bg-green-600 rounded-full shadow-md">
+            <Search className="h-6 w-6" />
+            <input
+              type="text"
+              placeholder="Search for a destination..."
+              className="w-full focus:outline-none bg-transparent placeholder-white"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
